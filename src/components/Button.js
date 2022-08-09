fc57698b6dd2b9dac062c4d3f6d88c1bbca307bc
Mingu Kim <mkim245@gmail.com>
2022-08-09 14:29:18 -0400

    refactor button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 import "components/Button.scss";
+import classNames from "classnames";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = classNames('button', {
+      'button--confirm': props.confirm,
+      'button--danger': props.danger
+   });
+
+   // if (props.confirm) {
+   //    buttonClass += ' button--confirm';
+   // }
+   // if (props.danger) {
+   //    buttonClass += ' button--danger';
+   // } line 7-10 is same as this if clause. https://web.compass.lighthouselabs.ca/days/w07d2/activities/1199
+
+   return (
+      <button 
+         className={buttonClass}
+         onClick={props.onClick}
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   );
 }
