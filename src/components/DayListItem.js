9f052ef775c2904bffc5ddecebe185f787e1641b
Mingu Kim <mkim245@gmail.com>
2022-08-10 17:08:09 -0400

    update daylist and interviewerlist files

---
+++ src/components/DayListItem.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+
+export default function DayListItem(props) {
+  let dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0
+ });
+  
+  const formatSpots = () => {
+    if(props.spots > 1) {
+      return `${props.spots} spots remaining`;
+    }
+    if (props.spots === 1) {
+      return `${props.spots} spot remaining`;
+    }
+    if (props.spots === 0) {
+      return `no spots remaining`;
+    }
+  }
+  return (
+    <li 
+      className = {dayClass} 
+      onClick={() => props.setDay(props.name)}
+    >
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  );
+}
