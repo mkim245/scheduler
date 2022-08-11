1bdd69d86b7ada7a58f133f0c6298f4f48f5918a
Mingu Kim <mkim245@gmail.com>
2022-08-10 23:25:46 -0400

    update status error in appointment

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,8 @@ import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
+import Status from "components/Appointment/Status.js";
+import Error from "components/Appointment/Error.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -161,3 +163,12 @@ storiesOf("InterviewerListItem", module)
         onConfirm={action("onConfirm")}
         onCancel={action("onCancel")}
         />)
+    .add("Status", () => 
+        <Status 
+        message={"Deleting"}
+    />)
+    .add("Error", () => 
+    <Error 
+    message={"Could not delete appointment"}
+    onClose={action("onClose")}
+/>)
