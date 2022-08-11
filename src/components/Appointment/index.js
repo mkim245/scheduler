7fe2de90a68dc415082e2157d6643834bc3460a6
Mingu Kim <mkim245@gmail.com>
2022-08-10 20:34:44 -0400

    init appointment index

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,11 @@
+// import Appointment from "components/Appointment";
+import React from "react";
+import "./styles.scss";
+
+export default function Appointment(props) {
+  return (
+    <article className="appointment">
+
+    </article>
+  );
+}
