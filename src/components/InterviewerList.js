bed63d2cada5e1e9e76841a0eb2c7424dbebfd26
Mingu Kim <mkim245@gmail.com>
2022-08-16 14:55:42 -0400

    no change

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,15 +5,16 @@ import InterviewerListItem from "./InterviewerListItem";
 export default function InterviewerList(props) {
   const InterviewerLists = props.interviewers.map(interviewer => {
     return (
-      <InterviewerListItem 
+      <InterviewerListItem
         key={interviewer.id}
-        name={interviewer.name} 
-        avatar={interviewer.avatar} 
+        name={interviewer.name}
+        avatar={interviewer.avatar}
         selected={interviewer.id === props.value}
         setInterviewer={() => props.onChange(interviewer.id)}
       />)
-  });
-  return (
+    });
+
+    return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">{InterviewerLists}</ul>
