af66803ab6cb37088a6230d54aefb7995fb6418e
Mingu Kim <mkim245@gmail.com>
2022-08-17 13:47:12 -0400

    use another better coverage

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -57,39 +57,24 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
   
-  it("calls onSave function when the name and interviewer is defined", () => {
-    /* 1. Create the mock onSave function */
+  it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-  
-    /* 2. Render the Form with interviewers, student name and the onSave mock function passed as an onSave prop */
-    const { getByText, queryByText } = render(
-      <Form
-        interviewers={interviewers}
-        onSave={onSave}
-        name="Lydia Miller-Jones"
-        interviewer={interviewers[0].id}
-      />
-    );
-  
-    /* 3. Click the save button */
-    fireEvent.click(getByText("Save"));
-  
-    expect(queryByText(/student name cannot be blank/i)).toBeNull();
-    expect(queryByText(/please select an interviewer/i)).toBeNull();
-    expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
-  });
-  
-  it("submits the name entered by the user", () => {
-    const onSave = jest.fn();
-    const { getByText, getByPlaceholderText } = render(
+    const { getByText, getByPlaceholderText, queryByText } = render(
       <Form interviewers={interviewers} onSave={onSave} interviewer={1} />
     );
   
-    const input = getByPlaceholderText("Enter Student Name");
-  
-    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
     fireEvent.click(getByText("Save"));
+  
+    expect(getByText(/Student name cannot be blank/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+  
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+  
+    fireEvent.click(getByText("Save"));
+  
+    expect(queryByText(/Student name cannot be blank/i)).toBeNull();
   
     expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
