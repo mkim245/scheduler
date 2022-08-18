52b1a4b95cd10002e3021964aeae646173cb5da6
Mingu Kim <mkim245@gmail.com>
2022-08-17 20:50:26 -0400

    add edit test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -63,7 +63,7 @@ describe("Application", () => {
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 
-  it.only("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
     // 1. Render the Application.
     const { container } = render(<Application />);
 
@@ -89,7 +89,39 @@ describe("Application", () => {
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
-  
+
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   });
+
+  it.only("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+  //  1. Render the Application.
+  const { container, debug } = render(<Application />);
+
+  // 2. Wait until the text "Archie Cohen" is displayed.
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+  const appointment = getAllByTestId(container, "appointment").find(
+    appointment => queryByText(appointment, "Archie Cohen")
+  );
+
+  fireEvent.click(queryByAltText(appointment, "Edit"));
+
+  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+    target: { value: "Lydia Miller-Jones" }
+  });
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+  fireEvent.click(getByText(appointment, "Save"));
+  expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+  await waitForElement(() => getByText(container, "Sylvia Palmer"))
+  // expect(getByText(container, "Sylvia Palmer")).toBeInTheDocument();
+
+  const day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+ 
+  expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+
+  debug();
+});
 });
